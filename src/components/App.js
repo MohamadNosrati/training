038c9a58a95be531ff7497f7bab6e31a777ecf47
mnst <mnst@mail.com>
2024-06-01 15:42:53 +0330

    first-commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle"
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-center bg-success'>this is test app!</h1>
+    </div>
+  )
+}
+
+export default App
+
+
